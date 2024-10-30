b083d9ce9d28028c6fcdd341849ba1c6c48a1e9c
JayapriyaS24 <sjayapriya2407@gmail.com>
2024-10-30 13:58:20 +0530

    commit added

---
+++ TailwindCLi/tailwind.config.js
@@ -0,0 +1,13 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+        fontFamily: {
+          Right: ['Righteous','sans-serif'],
+          Sai: ['Saira', 'sans-serif'],
+        }
+    },
+  },
+  plugins: [],
+}
